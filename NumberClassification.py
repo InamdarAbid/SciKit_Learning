455fd2b446f7a308054967875fc5345d132ec04b
InamdarAbid <2015abid.inamdar@ves.ac.in>
2018-12-18 19:21:59 +0530

    Digits recognition using SVM

--- NumberClassification.py
+++ NumberClassification.py
@@ -1,4 +1,20 @@
-import matplotlib 
-import numpy
-import sklearn
-print("HI")
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import svm
+
+#Load digits dataset
+digits = datasets.load_digits() 
+
+#Load SVM as clf
+clf = svm.SVC(gamma=0.001, C=100)
+
+#Divide data for training and testing
+X,y = digits.data[:-10], digits.target[:-10]
+
+#Fitting the data (Training)
+clf.fit(X,y)
+
+#Testing
+print(clf.predict(digits.data[-5:-4]))
+plt.imshow(digits.images[-5], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
