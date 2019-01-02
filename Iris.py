2a8eea958e11e55e54357e52c2bf37b550f388eb
InamdarAbid <2015abid.inamdar@ves.ac.in>
2019-01-02 20:49:20 +0530

    iris dataset Random Forest Completed

--- Iris.py
+++ Iris.py
@@ -10,3 +10,25 @@ np.random.seed(0)
 iris_data = load_iris()
 df = pd.DataFrame(iris_data.data, columns = iris_data.feature_names)
 print(df.head)
+
+df['species'] = pd.Categorical.from_codes(iris_data.target,iris_data.target_names)
+print(df.head())
+
+df['is_train'] = np.random.uniform(0,1,len(df)) <= 0.75
+print(df.head())
+
+#Divide into training and testing
+train = df[df['is_train'] == True]
+test = df[df['is_train'] == False]
+features = df.columns[:4]
+print(features)
+y = pd.factorize(train['species'])[0]
+print(y)
+clf = RandomForestClassifier(n_jobs = 2,random_state = 0)
+clf.fit(train[features],y)
+clf.predict(test[features])
+clf.predict_proba(test[features])[0:10]
+preds = iris_data.target_names[clf.predict(test[features])]
+preds[0:5]
+print(test['species'].head())
+print(pd.crosstab(test['species'],preds,rownames = ['Actual_Species'],colnames = ['Predicted_Species']))
